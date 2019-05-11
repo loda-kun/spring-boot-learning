c0d02f9663d78d6f9a5430dfb645cabf5d92c914
atomptit <41568621+atomptit@users.noreply.github.com>
2019-05-11 11:19:51 +0700

    refactor pom file

--- spring-cloud-config-server/src/main/java/me/loda/spring/springcloudconfig/ConfigServer.java
+++ spring-cloud-config-server/src/main/java/me/loda/spring/springcloudconfig/ConfigServer.java
@@ -1,13 +1,11 @@
-package me.loda;
+package me.loda.spring.springcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigServer
 @SpringBootApplication
-@ComponentScan("me.loda")
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
