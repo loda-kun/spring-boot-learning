d626b64218e88b293a77aed6db05698cdd1e92c0
loda <loda.namnh@gmail.com>
2019-05-12 22:58:20 +0700

    init componentscan module

---
+++ spring-boot-4-Component-Scan-@Controller/src/main/java/me/loda/spring/componentscan/App.java
@@ -0,0 +1,44 @@
+package me.loda.spring.componentscan;
+/*******************************************************
+ * For Vietnamese readers:
+ *    Các bạn thân mến, mình rất vui nếu project này giúp 
+ * ích được cho các bạn trong việc học tập và công việc. Nếu 
+ * bạn sử dụng lại toàn bộ hoặc một phần source code xin để 
+ * lại dường dẫn tới github hoặc tên tác giá.
+ *    Xin cảm ơn!
+ *******************************************************/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import me.loda.spring.componentscan.others.OtherGirl;
+
+/**
+ * Copyright 2019 {@author Loda} (https://loda.me).
+ * This project is licensed under the MIT license.
+ *
+ * @since 5/12/2019
+ * Github: https://github.com/loda-kun
+ */
+@SpringBootApplication(scanBasePackages = "me.loda.spring.componentscan.others")
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        try {
+            Girl girl = context.getBean(Girl.class);
+            System.out.println("Bean: " + girl.toString());
+        } catch (Exception e) {
+            System.out.println("Bean Girl không tồn tại");
+        }
+
+        try {
+            OtherGirl otherGirl = context.getBean(OtherGirl.class);
+            if (otherGirl != null) {
+                System.out.println("Bean: " + otherGirl.toString());
+            }
+        } catch (Exception e) {
+            System.out.println("Bean Girl không tồn tại");
+        }
+    }
+}
---
+++ spring-boot-4-Component-Scan-@Controller/src/main/java/me/loda/spring/componentscan/Girl.java
@@ -0,0 +1,27 @@
+package me.loda.spring.componentscan;
+/*******************************************************
+ * For Vietnamese readers:
+ *    Các bạn thân mến, mình rất vui nếu project này giúp 
+ * ích được cho các bạn trong việc học tập và công việc. Nếu 
+ * bạn sử dụng lại toàn bộ hoặc một phần source code xin để 
+ * lại dường dẫn tới github hoặc tên tác giá.
+ *    Xin cảm ơn!
+ *******************************************************/
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Copyright 2019 {@author Loda} (https://loda.me).
+ * This project is licensed under the MIT license.
+ *
+ * @since 5/12/2019
+ * Github: https://github.com/loda-kun
+ */
+@Component
+public class Girl {
+
+    @Override
+    public String toString() {
+        return "Girl.java";
+    }
+}
