fcef89b31466bd8dc5b48c83c0fb6b0aeb5cc7eb
loda <loda.namnh@gmail.com>
2019-06-03 22:30:56 +0700

    add conditional 2 :cherries:

---
+++ spring-boot-@Conditional-2-custom-conditional/src/main/java/me/loda/spring/customconditional/AppConfiguration.java
@@ -0,0 +1,36 @@
+package me.loda.spring.customconditional;
+/*******************************************************
+ * For Vietnamese readers:
+ *    Các bạn thân mến, mình rất vui nếu project này giúp 
+ * ích được cho các bạn trong việc học tập và công việc. Nếu 
+ * bạn sử dụng lại toàn bộ hoặc một phần source code xin để 
+ * lại dường dẫn tới github hoặc tên tác giá.
+ *    Xin cảm ơn!
+ *******************************************************/
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Copyright 2019 {@author Loda} (https://loda.me).
+ * This project is licensed under the MIT license.
+ *
+ * @since 6/3/2019
+ * Github: https://github.com/loda-kun
+ */
+@Configuration
+public class AppConfiguration {
+    public static class SomeBean{
+    }
+
+    /*
+    SomeBean chỉ được tạo ra khi
+    thỏa mãn điều kiện của WindowRequired
+     */
+    @Conditional(WindowRequired.class)
+    @Bean
+    SomeBean someBean(){
+        return new SomeBean();
+    }
+}
---
+++ spring-boot-@Conditional-2-custom-conditional/src/main/java/me/loda/spring/customconditional/WindowRequired.java
@@ -0,0 +1,32 @@
+package me.loda.spring.customconditional;
+/*******************************************************
+ * For Vietnamese readers:
+ *    Các bạn thân mến, mình rất vui nếu project này giúp 
+ * ích được cho các bạn trong việc học tập và công việc. Nếu 
+ * bạn sử dụng lại toàn bộ hoặc một phần source code xin để 
+ * lại dường dẫn tới github hoặc tên tác giá.
+ *    Xin cảm ơn!
+ *******************************************************/
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * Copyright 2019 {@author Loda} (https://loda.me).
+ * This project is licensed under the MIT license.
+ *
+ * @since 6/3/2019
+ * Github: https://github.com/loda-kun
+ */
+/*
+Một điều kiện, phải kế thừa lớp Condition của Spring Boot cung cấp
+ */
+public class WindowRequired implements Condition{
+
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        // Nếu OS ra window trả ra true.
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+}
