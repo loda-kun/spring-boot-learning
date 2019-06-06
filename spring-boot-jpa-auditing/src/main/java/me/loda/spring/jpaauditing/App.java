3570beae2e4a04839a5fee29b0aadd4ff79c5639
Nguyen Hoang Nam <nguyen.hoang.nam@linecorp.com>
2019-06-06 17:06:27 +0700

    add application context event :atm:

---
+++ spring-boot-jpa-auditing/src/main/java/me/loda/spring/jpaauditing/App.java
@@ -0,0 +1,18 @@
+package me.loda.spring.jpaauditing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright 2019 {@author Loda} (https://loda.me).
+ * This project is licensed under the MIT license.
+ *
+ * @since 2019-06-06
+ * Github: https://github.com/loda-kun
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
