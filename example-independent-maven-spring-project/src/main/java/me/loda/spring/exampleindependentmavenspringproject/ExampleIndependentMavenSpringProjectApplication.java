0022c06741d987b58492198bdf11bc30f80268f7
Nguyen Hoang Nam <nguyenhoangnam@VN-LV00141-N.local>
2019-12-05 14:59:10 +0700

    Update more details in pom.xml :hamburger:

---
+++ example-independent-maven-spring-project/src/main/java/me/loda/spring/exampleindependentmavenspringproject/ExampleIndependentMavenSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package me.loda.spring.exampleindependentmavenspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleIndependentMavenSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleIndependentMavenSpringProjectApplication.class, args);
+	}
+
+}
---
+++ example-independent-maven-spring-project/src/test/java/me/loda/spring/exampleindependentmavenspringproject/ExampleIndependentMavenSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.exampleindependentmavenspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleIndependentMavenSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
