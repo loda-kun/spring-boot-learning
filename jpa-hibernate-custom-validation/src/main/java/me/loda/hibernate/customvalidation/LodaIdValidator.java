107c26f550e3f85bf5eee8aac8650f7b9938df98
Nguyen Hoang Nam <loda.namnh@gmail.com>
2019-12-19 17:49:23 +0700

    hibernate-validator :baby_symbol: :

--- jpa-hibernate-custom-validation/src/main/java/me/loda/hibernate/customvalidation/LodaIdValidator.java
+++ jpa-hibernate-custom-validation/src/main/java/me/loda/hibernate/customvalidation/LodaIdValidator.java
@@ -6,6 +6,12 @@ import javax.validation.ConstraintValidatorContext;
 public class LodaIdValidator implements ConstraintValidator<LodaId, String> {
     private static final String LODA_PREFIX = "loda://";
 
+    /**
+     * Kiểm tra tính hợp lệ của trường được đánh dấu bởi @LodaId
+     * @param s
+     * @param constraintValidatorContext
+     * @return
+     */
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
         if (s == null || s.isEmpty()) return false;
